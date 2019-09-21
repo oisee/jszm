25504492eaccc601da79d13da57d34d67b2cf222
Steven Hugg <hugg@fasterlight.com>
2019-09-20 23:01:23 -0500

    refactor to get around TickObject explosion with generators

--- search.js
+++ search.js
@@ -39,19 +39,20 @@ class Playthrough {
   }
 }
 
-G.run((function*() {
-  var story=yield fs.readFileG(process.argv[2],{});
-  var game=new JSZM(story);
+var story=fs.readFileSync(process.argv[2]);
+var game=new JSZM(story);
+
+function GameRunner() {
+
   var vocab;		// vocabulary for game
   var vocabdict = {};	// .. in dictionary form
-  var maxturns = 30;	// max turns in current game
+  var maxturns = 50;	// max turns in current game
   var usewords = false; // use word output as tokens?
   var usetech = true;   // use vm tech output?
   var prob_vocab = 0.5; // probability of a recent vocab word
   var prob_end = 0.5;   // probability of ending the command
 
   var numplays = 0;	// # of plays in all games
-  var allcmdstats = {};	// command -> record
   var ignorecmds = {}; // no longer used JSON.parse(fs.readFileSync('ignorecmds.json'));
   var playthru;		// current game Playthrough
   var alltokstats = {}; // token -> record
@@ -92,7 +93,7 @@ G.run((function*() {
     if (a == 'pf') return; // use as evidence of activity, but don't record
     if (usetech) logtoken(a+"_"+b+"_"+c);
   }
-  function newgame() {
+  this.newgame = function() {
     // reset other stuff
     playtoks = new Set();
     playvocab = [];
@@ -117,19 +118,22 @@ G.run((function*() {
     if (!goalmet) {
       goalrec.goalsucc += 1;
       console.log("Goal success:",goaltok,goalrec.goalsucc,'/',goalrec.goalruns,'turn #',numturns);
-      var oldpriorcount = goalrec.priorcount | 0;
-      var newpriors = playthru.merge(goalrec.best, goalrec.first, numturns);
-      if (oldpriorcount == newpriors.size) {
-        goalrec.priorstable += 1;
-      } else {
-        goalrec.priorstable = 0;
+      // if we're already stable, stop testing
+      if (goalrec.priorstable < 100) {
+        var oldpriorcount = goalrec.priorcount | 0;
+        var newpriors = playthru.merge(goalrec.best, goalrec.first, numturns);
+        if (oldpriorcount == newpriors.size) {
+          goalrec.priorstable += 1;
+        } else {
+          goalrec.priorstable = 0;
+        }
+        goalrec.priorcount = newpriors.size;
+        console.log('MERGE', goaltok, oldpriorcount, '->', newpriors.size, '/', goalrec.priorstable);
       }
-      goalrec.priorcount = newpriors.size;
-      console.log('MERGE', goaltok, oldpriorcount, '->', newpriors.size, '/', goalrec.priorstable);
       goalmet = 1;
     }
   }
-  function endgame() {
+  this.endgame = function() {
     // did we not meet goal?
     if (goaltok && !goalmet) {
       console.log("Goal failure:",goaltok,goalrec.goalsucc,'/',goalrec.goalruns);
@@ -161,15 +165,6 @@ G.run((function*() {
         console.log("IGNORING", turncmd);
       }
     } else {
-      // create command stats
-      var verb = turncmd; //.split(' ')[0];
-      let thiscmdstats = allcmdstats[verb];
-      if (!thiscmdstats)
-        thiscmdstats = allcmdstats[verb] = {
-          cmd:verb,
-          toks:{},
-          score:0
-        };
       // look at all tokens for this turn
       for (let token of turntoks) {
         let stat = alltokstats[token];
@@ -189,13 +184,7 @@ G.run((function*() {
           stat.first = numturns;
           stat.cmd = turncmd;
         }
-        // new token for this cmd?
-        if (!thiscmdstats.toks[token]) {
-          thiscmdstats.toks[token] = 1;
-          thiscmdstats.score += 1; // TODO: not used
-        }
       }
-      //console.log(turncmd,'+',turnscore,'=',thiscmdstats.score);
       // add to playtoks
       for (let token of turntoks) {
         playtoks.add(token);
@@ -308,25 +297,31 @@ G.run((function*() {
       return null;
     }
   };
-  while (1) {
+}
+
+var runner = new GameRunner();
+
+function*GrunOne() {
+  {
     try {
-      newgame();
+      runner.newgame();
       yield*game.run();
     } catch(e) {
       if (e instanceof DeadError) {
-        console.log(turncmd,'killed you');
-        delete allcmdstats[turncmd]; // TODO?
-        if (0) {
-          fs.writeFileSync('ignorecmds.json.tmp',JSON.stringify(ignorecmds));
-          fs.renameSync('ignorecmds.json.tmp', 'ignorecmds.json');
-        }
+        //console.log(turncmd,'killed you');
       } else if (e instanceof NoMoreTurns) {
         //
       } else {
         throw e;
       }
     }
-    endgame();
+    runner.endgame();
   }
-  process.exit(0);
-})());
+  process.nextTick(runOne);
+}
+
+function runOne() {
+  G.run(GrunOne);
+}
+process.nextTick(runOne);
+
